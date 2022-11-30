963e716e34bf41e7c02502146c9eae5af7afb171
mikachi20 <64882995+mikachi20@users.noreply.github.com>
2022-11-30 14:21:53 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,13 @@ import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  // key
+  apiKey: "AIzaSyAotYcHTdQ7MJe-Xh9Fd6kGOqp3ErlRiaA",
+  authDomain: "baum-test-5f17d.firebaseapp.com",
+  projectId: "baum-test-5f17d",
+  storageBucket: "baum-test-5f17d.appspot.com",
+  messagingSenderId: "24936556294",
+  appId: "1:24936556294:web:0b2b2d4d5b0dc2f5c8e087",
+  measurementId: "G-3NS4477JJ8",
 }
 
 // Initialize Firebase
